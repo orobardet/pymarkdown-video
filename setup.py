1c6df09879b2fa5404dd8cb69193f9b1ecd2db81
Olivier Robardet <olivier.robardet@gmail.com>
2020-08-17 11:27:54 +0200

    :rocket: Publish package to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
     name='pymarkdown-video',
-    version='1.0',
+    version='1.0.0',
+    description='Render video tags in python markdown for images that are video',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Olivier Robardet',
+    author_email='orobardet@gmail.com',
+    keywords="markdown image video",
+    license='MIT',
+    url='https://github.com/orobardet/pymarkdown-video',
     py_modules=['pymarkdown-video'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires = ['markdown>=3.0'],
+    python_requires='>=3',
 )
